198c2d5e2303fc1d40ba0f29e364931dbf82760f
Miguel Cabrera <miguelcabsan@gmail.com>
2020-06-18 22:50:15 +0100

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/miguelignaciocabrerasan-gil/Development/Gatsby/mcabsan-site/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/miguelignaciocabrerasan-gil/Development/Gatsby/mcabsan-site/src/pages/index.js")))
+}
+
