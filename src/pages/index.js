c9c19ea297420839c75c0552081a301dcf0d4f94
Miguel Cabrera <miguelcabsan@gmail.com>
2020-06-21 22:00:10 +0100

    feat(index): Update info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
-import React from "react"
-import { Styled } from "theme-ui"
+/** @jsx jsx */
+import { jsx, Avatar } from "theme-ui"
+
 import Layout from "../components/layout"
+import me from "../assets/me.jpg"
 
 const Home = () => {
   return (
     <Layout>
-      <Styled.div>Hola mundo</Styled.div>
+      <div sx={{ textAlign: "center" }}>
+        <Avatar src={me} width="256px" height="256px" />
+        <h3>Hi! My name is Miguel Ignacio Cabrera San Gil</h3>
+      </div>
     </Layout>
   )
 }
