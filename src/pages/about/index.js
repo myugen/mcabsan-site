b1a9483f510d33ac45e23154d1a027d75d9ec120
Miguel Cabrera <miguelcabsan@gmail.com>
2020-09-21 02:28:43 +0100

    feat(title): Added page title metadata

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,7 +5,7 @@ import Layout from "components/layout"
 
 const About = () => {
   return (
-    <Layout>
+    <Layout metadata={{ title: "About" }}>
       <div>
         <h3>This is About info!</h3>
       </div>
