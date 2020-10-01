12e3d80c97e1b0867cdc50cbf38c48410ffc9e4a
Miguel Cabrera <miguelcabsan@gmail.com>
2020-10-01 16:57:51 +0100

    fix(api): Fix gatsby graphql problem

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,8 @@
 import axios from "axios"
-import { graphql } from "gatsby"
 import { environment } from "utils"
 
 const { isDev, port } = environment
-console.log("ENV", port)
-const { site } = graphql`
-  query SiteMetadata {
-    site {
-      siteMetadata {
-        title
-        menuLinks {
-          name
-          path
-        }
-        description
-        author
-        location
-        siteUrl
-        image
-        social {
-          twitter {
-            user
-            url
-          }
-          github {
-            user
-            url
-          }
-          linkedin {
-            user
-            url
-          }
-        }
-        googleFonts
-        keywords
-      }
-    }
-  }
-`
+// FIX: Erase baseURL hardcode
 export default axios.create({
-  baseURL: isDev ? `http://localhost:${port}` : site.siteMetadata.siteUrl,
+  baseURL: isDev ? `http://localhost:${port}` : "https://mcabsan.dev",
 })
