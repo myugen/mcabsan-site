636234b03c791468680c9eb170932e4dea2be5bf
Miguel Cabrera <miguelcabsan@gmail.com>
2020-10-06 00:47:12 +0100

    fix(env): Fix env vars for client side

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,9 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <GoogleReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}>
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.GATSBY_RECAPTCHA_SITE_KEY}
+    >
       {element}
     </GoogleReCaptchaProvider>
   )
