2551c7a0bf7d0b5c130d29ddfb2f93c0769eb666
Miguel Cabrera <miguelcabsan@gmail.com>
2020-10-11 15:38:53 +0100

    improvement(style): Fix some style issues

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,5 +3,6 @@ import Switch from "./switch"
 import ScrollToTop from "./scroll-to-top"
 import ContactForm from "./contact-form"
 import RecaptchaBanner from "./recaptcha-banner"
+import Toast from "./toast"
 
-export { SocialSpace, Switch, ScrollToTop, ContactForm, RecaptchaBanner }
+export { SocialSpace, Switch, ScrollToTop, ContactForm, RecaptchaBanner, Toast }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ const Home = () => {
     <Layout metadata={{ title: "Home" }}>
       <div sx={{ textAlign: "center" }}>
         <Avatar src={me} width="256px" height="256px" />
-        <h3>Hi! My name is Miguel Cabrera</h3>
+        <h3 sx={{ variant: "text.title" }}>Hi! I'm Miguel Cabrera</h3>
         <p>
-          I am a passionate fullstack developer, who loves to be involved in the
-          whole process of creating software to keep learning and facing new
-          challenges every day.
+          I am a passionate fullstack developer based in Tenerife, Canary
+          Island, who loves to be involved in the whole process of creating
+          software to keep learning and facing new challenges every day.
         </p>
       </div>
     </Layout>
